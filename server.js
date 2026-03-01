f51a7899dc42a4763481b773b692428acfa1211c
girish-69 <yeuha.lama.na@gmail.com>
2026-03-01 11:29:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("Kabutr backend running ✅"));
+
 app.get("/health", (req, res) => {
   res.json({ ok: true });
 });
